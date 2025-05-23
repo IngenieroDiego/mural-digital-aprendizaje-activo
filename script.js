f76f7d333ead076f820eedbc974ed8c70690d73d
Diego Fuentes Garcia <114308272+IngenieroDiego@users.noreply.github.com>
2025-05-22 21:21:01 -0500

    Version 1.0

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.textContent = "Mural diseñado por Diego Fuentes, Edwin Antonio González Ortiz y Eder Fabián Trujillo Jiménez – 2025";
+}
